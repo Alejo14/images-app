1aab2d63aa0d823eaa2ccf9b6d845b9af6548a27
Alejandro Tapia <a.tapiat@pucp.pe>
2024-10-28 02:39:24 -0500

    [FEAT] Adding go home functionality

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,8 +1,20 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
 const Header: React.FC = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/');
+    }
+
     return (
-        <section className="header w-full p-4 text-left">
-            <h1 className="mb-1">Photo Search</h1>
-            <p>Alejandro Tapia</p>
+        <section className="flex justify-between items-center header w-full px-10">
+            <div className="p-4 text-left">
+                <h1 className="mb-1">Photo Search</h1>
+                <p>Alejandro Tapia</p>
+            </div>
+            {location.pathname !== '/' && <a onClick={handleClick} href="" className="cursor-pointer text-white hover:text-black">Home</a>}
         </section>
     )
 }
